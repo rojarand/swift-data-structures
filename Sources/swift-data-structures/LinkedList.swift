f248d541a29791e4553222cfaf12de02f1bd076e
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-22 18:57:42 +0100

    Remove duplicates from linked list (#6)

--- Sources/swift-data-structures/LinkedList.swift
+++ Sources/swift-data-structures/LinkedList.swift
@@ -5,9 +5,31 @@
 //  Created by Robert Andrzejczyk on 21/03/2023.
 //
 
-public class SinglyLinkedList<T> {
+class Node<T> {
+    let data: T
+    var next: Node<T>?
     
-    public var head: Node<T>?
+    init(data: T, next: Node<T>? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+public class LinkedList<T> {
+    
+    var head: Node<T>?
+    
+    init(_ elements : T...) {
+        if let first = elements.first {
+            var last = Node(data: first)
+            head = last
+            elements.dropFirst(1).forEach { element in
+                let next = Node(data: element)
+                last.next = next
+                last = next
+            }
+        }
+    }
     
     public func append(data: T) {
         let newNode = Node(data: data)
@@ -19,16 +41,13 @@ public class SinglyLinkedList<T> {
     }
     
     public var count: UInt {
-        if var runner = head {
-            var count: UInt = 1
-            while let next = runner.next {
-                count += 1
-                runner = next
-            }
-            return count
-        } else {
-            return 0
+        var runner = head
+        var count: UInt = 0
+        while let node = runner {
+            count += 1
+            runner = node.next
         }
+        return count
     }
     
     private var last: Node<T>? {
@@ -42,12 +61,73 @@ public class SinglyLinkedList<T> {
     }
 }
 
-public class Node<T> {
-    public let data: T
-    public var next: Node<T>?
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+extension LinkedList: Sequence {
     
-    init(data: T, next: Node<T>? = nil) {
-        self.data = data
-        self.next = next
+    public func makeIterator() -> LinkedListIterator<T> {
+        LinkedListIterator(head: head)
+    }
+    
+    /*
+    public func makeIterator() -> some IteratorProtocol {
+        LinkedListIterator(head: head)
+    }
+     */
+}
+
+public struct LinkedListIterator<T>: IteratorProtocol {
+    
+    private var currentNode: Node<T>?
+    
+    init(head: Node<T>? = nil) {
+        self.currentNode = head
+    }
+    
+    public mutating func next() -> T? {
+        if let node = currentNode {
+            currentNode = node.next
+            return node.data
+        } else {
+            return nil
+        }
+    }
+}
+
+extension LinkedList where T:Hashable {
+    
+    func removeDuplicates() {
+        var elements: Set<T> = []
+        if let head {
+            elements.insert(head.data)
+        }
+        var runner = head
+        while let r = runner {
+            if let next = r.next {
+                if elements.contains(next.data) {
+                    r.next = next.next
+                } else {
+                    elements.insert(next.data)
+                    runner = r.next
+                }
+            } else {
+                runner = nil
+            }
+        }
+    }
+    
+    func removeDuplicates2() {
+        var runner1 = head
+        while let r1 = runner1 {
+            var runner2: Node<T>? = r1
+            while let r2 = runner2, let next = r2.next {
+                if r1.data == next.data {
+                    r2.next = next.next
+                } else {
+                    runner2 = r2.next
+                }
+            }
+            runner1 = r1.next
+        }
     }
 }
